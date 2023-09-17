9bcbe42b642b8c24ba174f7a468fa50dd6730a2a
AlexandrTurdiyev <alexandr.turdiyev@gmail.com>
2023-09-17 23:23:48 +0530

    feat: update sidebar for closed if click on overlay and links and burger button

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,9 +1,37 @@
+const body = document.body;
+const overlay = document.querySelector('.overlay');
 const  burger = document?.querySelector('[data-burger]');
 const sidebar = document.querySelector('.page__sidebar-wrapper');
-console.log(burger);
-console.log(sidebar);
+const sidebarLinks = document.querySelectorAll('.sidebar__link');
 
-burger.addEventListener('click', () => {
+function toggleClassesForBurger () {
     burger.classList.toggle('burger--active');
     sidebar.classList.toggle('element--visible');
+    body.classList.toggle('stop-scroll');
+    overlay.classList.toggle('overlay--active');
+}
+burger.addEventListener('click', () => {
+    toggleClassesForBurger();
+})
+
+sidebarLinks.forEach(elem => {
+    elem.addEventListener('click', () => {
+        ifBurgerMenuOpen();
+    })
+})
+
+function ifBurgerMenuOpen () {
+    if (overlay.classList.contains('overlay--active')) {
+        if (burger.classList.contains('burger--active')) {
+            if (sidebar.classList.contains('element--visible')) {
+                if (body.classList.contains('stop-scroll')) {
+                    toggleClassesForBurger();
+                }
+            }
+        }
+    }
+}
+
+overlay.addEventListener('click', () => {
+    ifBurgerMenuOpen();
 })
